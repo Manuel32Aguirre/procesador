f6e2a21fd92eace1b6d720ee821e6aaa1379070e
Manuel <programadorjrsemad@gmail.com>
2024-11-15 11:17:45 -0600

    Nueva versión

--- CODIGO.asm
+++ CODIGO.asm
@@ -1,6 +1,8 @@
-def temp = rjmp
-def cte=r17
-
+.def temp = r16
+.def cte=r17
+.def cont1 = r18
+.def cont2 = r19
+.def cont3 = r20
 .cseg
 .org 0
 
@@ -8,6 +10,7 @@ def cte=r17
 ldi temp $00
 ldi cte, $01
 out portb, cte
+rcall delay_125m
 izquierda: add temp, cte
 ldi cte, $$80
 izquierda2: lsl temp
@@ -22,7 +25,41 @@ derecha: add temp, cte
 ldi cte, $01
 derecha2: lsr temp
 out portb, temp
+rcall delay_125m
 sub temp, cte
 breq izquierda
 add temp, cte
 rjmp derecha2
+
+delay_125m: ldi cont1, 5
+ldi cont2, 200
+lazo2: ldi cont3, 200
+lazo1: nop
+nop
+nop
+nop
+nop
+nop
+nop
+dec cont 3 //No tenemos esta instruccion, hay que implementarla, USAMOS RESTA
+brne lazo1
+dec cont2
+brne lazo2
+dec cont1
+brne lazo3
+ret
+
+/*Proyecto
+Hacer un contaador ascendente o descendente
+queremos veerlo en los display que traen las tarjetas
+si nop traen display, lo ponemos
+
+que llegue a 999
+minimo 3 display
+necesitamos un puerto para los segmentos
+CA, CB,,...CF
+Hay que multiplexar
+agregar otro puerto
+
+Que operaciones
+*/
